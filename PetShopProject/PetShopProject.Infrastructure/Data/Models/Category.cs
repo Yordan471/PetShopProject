4d055480d7d3305de5ff593a9904a98ea4a0f389
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-02-27 20:47:44 +0200

    Adding Product and Category entities in folder Models in Infrastructure layer

---
+++ PetShopProject/PetShopProject.Infrastructure/Data/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static PetShopProject.Common.EntityValidationsConstants.CategoryValidations;
+
+namespace PetShopProject.Infrastructure.Data.Models
+{
+    [Comment("Product Category")]
+    public class Category
+    {
+        [Comment("Category identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Categiry name")]
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ PetShopProject/PetShopProject.Infrastructure/Data/Models/Product.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static PetShopProject.Common.EntityValidationsConstants.ProductValidations;
+
+namespace PetShopProject.Infrastructure.Data.Models
+{
+    [Comment("Animal product")]
+    public class Product
+    {
+        [Comment("Product identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Product name")]
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Comment("Product description")]
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = string.Empty;
+
+        [Comment("Product price")]
+        [Required]
+        public decimal Price { get; set; }
+
+        [Comment("Product image path")]
+        [Required]
+        [Url]
+        [MaxLength(ImageUrlMaxLength)]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [Comment("Category identifier")]
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Comment("Category entity")]
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; } = null!;
+    }
+}
