c6a3087bf3c972691661702fbc79d6f394369529
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-03-01 12:16:01 +0200

    Adding Order and OrderItem models + properties, comments and DataAnotations

---
+++ PetShopProject/PetShopProject.Infrastructure/Data/Models/Order.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Infrastructure.Data.Models
+{
+
+    [Comment("Order of product/s")]
+    public class Order
+    {
+        [Comment("Order identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("User identifier")]
+        [Required]
+        public int UserId { get; set; }
+
+        [Comment("User entity")]
+        public IdentityUser User { get; set; }
+
+        [Comment("Date time of order")]
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Comment("Total price of order")]
+        [Required]
+        public decimal TotalAmount { get; set; }
+
+        [Comment("Order is completed or not")]
+        [Required]
+        public bool IsCompleted { get; set; } 
+    }
+}
---
+++ PetShopProject/PetShopProject.Infrastructure/Data/Models/OrderDetails.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Infrastructure.Data.Models
+{
+    [Comment("Details of the order")]
+    public class OrderItem
+    {
+        [Comment("Order details identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Order identifier")]
+        [Required]
+        public int OrderId { get; set; }
+
+        [Comment("Order entity")]
+        [ForeignKey(nameof(OrderId))]
+        public Order Order { get; set; } = null!;
+
+        [Comment("Product identifier")]
+        [Required]
+        public int ProductId { get; set; }
+
+        [Comment("Product entity")]
+        public Product Product { get; set; } = null!;
+
+        [Comment("Quantity of units")]
+        [Required]
+        public int Quantity { get; set; }
+
+        [Comment("Price for a unit ot product")]
+        [Required]
+        public decimal UnitPrice { get; set; }
+    }
+}
