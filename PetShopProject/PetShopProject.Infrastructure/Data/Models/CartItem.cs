49cf2b168448497d8493f365ffd610d83a2f231e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-03-01 13:29:42 +0200

    Addint CartItem and Comment model + Properties + DataAnnotations + Comments and validations

---
+++ PetShopProject/PetShopProject.Infrastructure/Data/Models/CartItem.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Infrastructure.Data.Models
+{
+    [Comment("Cart with item")]
+    public class CartItem
+    {
+        [Comment("Cart item identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Custummer identifier")]
+        [Required]
+        public int CustrummerId { get; set; }
+
+        [Comment("Custummer entity")]
+        public IdentityUser Custummer { get; set; } = null!;
+
+        [Comment("Product identifier")]
+        [Required]
+        public int ProductId { get; set; }
+
+        [Comment("Product entity")]
+        [ForeignKey(nameof(ProductId))]
+        public Product Product { get; set; } = null!;
+
+        [Comment("Quantity of a product")]
+        [Required]
+        public int Quantity { get; set; }
+
+        [Comment("Price of a single unit for product")]
+        [Required]
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ PetShopProject/PetShopProject.Infrastructure/Data/Models/Comment.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static PetShopProject.Common.EntityValidationsConstants.CommentValidations;
+
+namespace PetShopProject.Infrastructure.Data.Models
+{
+    [Comment("User comment")]
+    public class Comment
+    {
+        [Comment("Comment identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Post identifier")]
+        [Required]
+        public int PostId { get; set; }
+
+        [Comment("Blog post entity")]
+        [ForeignKey(nameof(PostId))]
+        public BlogPost BlogPost { get; set; } = null!;
+
+        [Comment("User identifier")]
+        [Required]
+        public int UserId { get; set; }
+
+        [Comment("User entity")]
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser User { get; set; } = null!;
+
+        [Comment("Comment content")]
+        [Required]
+        [MaxLength(ContentMaxValue)]
+        public string Content { get; set; } = null!;
+    }
+}
