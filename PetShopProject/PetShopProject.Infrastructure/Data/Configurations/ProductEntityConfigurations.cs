beaa613f4b943fc6d7143c9839e7ab6bed41b15b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-03-08 11:34:00 +0200

    Adding configurations for Product entity

---
+++ PetShopProject/PetShopProject.Infrastructure/Data/Configurations/ProductEntityConfigurations.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetShopProject.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Infrastructure.Data.Configurations
+{
+    public class ProductEntityConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(c => c.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasMany(p => p.OrdersDetails)
+                .WithOne(od => od.Product)
+                .HasForeignKey(od => od.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasMany(p => p.Comments)
+                .WithOne(c => c.Product)
+                .HasForeignKey(c => c.ProductId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
