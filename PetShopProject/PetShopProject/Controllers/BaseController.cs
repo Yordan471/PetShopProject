6af0330211e05ebdfe5ad9ace455aecd8e3aa24a
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-03-16 18:26:34 +0200

    Adding BaseController with Authorize attribute; Adding UserController for all IdentityUser Actions. Adding Register Action

---
+++ PetShopProject/PetShopProject/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetShopProject.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {       
+    }
+}
---
+++ PetShopProject/PetShopProject/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PetShopProject.Infrastructure.Data.Models;
+
+namespace PetShopProject.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly UserManager<User> userManager;
+
+        private readonly SignInManager<User> signInManager;
+
+        public UserController(
+            UserManager<User> _userManager,
+            SignInManager<User> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+
+
+            return View();
+        }
+    }
+}
