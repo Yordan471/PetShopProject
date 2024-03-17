c7f87b4f2af71432e4f0dcee4646a0c60498c849
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-03-17 18:56:24 +0200

    Adding Register Action httpget and httpPost

--- PetShopProject/PetShopProject/Controllers/UserController.cs
+++ PetShopProject/PetShopProject/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PetShopProject.Infrastructure.Data.Models;
+using PetShopProject.ViewModels.UserViewModels;
 
 namespace PetShopProject.Controllers
 {
@@ -28,9 +29,37 @@ namespace PetShopProject.Controllers
                 return RedirectToAction("Index", "Home");
             }
 
-
-
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new User
+            { 
+                UserName = model.LastName,
+                Email = model.Email 
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Login", "User");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }  
+
+            return View(model);
         }
     }
 }
