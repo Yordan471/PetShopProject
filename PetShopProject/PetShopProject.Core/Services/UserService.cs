272d2d9d98c2ac796a183993e257f6eb6c81adfd
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-03-17 18:57:44 +0200

    Adding contracts and services in Core layer for bussines logic

---
+++ PetShopProject/PetShopProject.Core/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using PetShopProject.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Core.Services
+{
+    public class UserService : IUserService
+    {
+    }
+}
