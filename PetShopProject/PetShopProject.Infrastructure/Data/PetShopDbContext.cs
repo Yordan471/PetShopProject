778e89e294eeb2ed3e723f62e215b1677bd54e7d
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-03-24 21:06:40 +0200

    Deleting DbSet Users and adding migration + Update database

--- PetShopProject/PetShopProject.Infrastructure/Data/PetShopDbContext.cs
+++ PetShopProject/PetShopProject.Infrastructure/Data/PetShopDbContext.cs
@@ -31,8 +31,6 @@ namespace PetShopProject.Infrastructure.Data
 
         public DbSet<Tag> Tags { get; set; }
 
-        public DbSet<User> Users { get; set; }
-
         public DbSet<Address> Addresses { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
