0ba8bfc29b2b980b4b30b9c3a9d7c4e9e55e7034
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-03-24 14:24:11 +0200

    Adding the database anew

---
+++ PetShopProject/PetShopProject.Infrastructure/Migrations/20240324114446_AddEntitiesAnewWithConfigurationsAndAddressEntity.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetShopProject.Infrastructure.Migrations
+{
+    public partial class AddEntitiesAnewWithConfigurationsAndAddressEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AddressId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                comment: "Address identifier");
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Address identifier")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    City = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false, comment: "City for registration"),
+                    Street = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false, comment: "Street for registration"),
+                    PostalCode = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false, comment: "Postal code for registration")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                },
+                comment: "Address entity");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_AddressId",
+                table: "AspNetUsers",
+                column: "AddressId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Addresses_AddressId",
+                table: "AspNetUsers",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Addresses_AddressId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_AddressId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "AddressId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                comment: "User's address");
+        }
+    }
+}
