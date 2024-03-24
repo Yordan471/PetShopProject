ede9f96e18105843762ab2fb6bcc6653501e51cc
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-03-24 21:09:09 +0200

    Adding Details action in Product and Category Controllers + Index action for Category controller + implementation

---
+++ PetShopProject/PetShopProject/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetShopProject.Core.Contracts;
+using PetShopProject.Infrastructure.Data.Models;
+using PetShopProject.ViewModels.CategoryViewModels;
+using PetShopProject.ViewModels.ProductViewModels;
+
+
+namespace PetShopProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IProductService productService;
+        private readonly ICategoryService categoryService;
+
+        public CategoryController(ICategoryService _categoryService, IProductService _productService)
+        {
+            this.productService = _productService;
+            this.categoryService = _categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<CategoryViewModel> categories = await categoryService.GetAllCategoriesAsync();
+
+            if (categories == null)
+            {
+                return NotFound();
+            }
+
+            return View(categories);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            CategoryViewModel category = await categoryService.GetCategoryByIdAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            IEnumerable<ProductViewModel> products = await productService.GetAllProductsOfCategoryAsync(id);
+
+            if (products != null && products.Any())
+            {
+                category.Products = products;
+            }
+
+            return View(category);
+        }
+    }
+}
---
+++ PetShopProject/PetShopProject/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetShopProject.Core.Contracts;
+
+namespace PetShopProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService _productService)
+        {
+            this.productService = _productService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await productService.GetProductByIdAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+    }
+}
