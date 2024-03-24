843326e0a36d3385b71c1874b600189b82a08912
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-03-24 21:11:22 +0200

    Adding CategoryViewModel, ProductViewModel

---
+++ PetShopProject/PetShopProject.Core/ViewModels/CategoryViewModels/CategoryViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetShopProject.ViewModels.ProductViewModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetShopProject.ViewModels.CategoryViewModels
+{
+    public class CategoryViewModel
+    {
+        public CategoryViewModel() 
+        {
+            Products = new HashSet<ProductViewModel>();
+        }
+
+        /// <summary>
+        /// Category identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Category name
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Category description
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// All products of a category
+        /// </summary>
+        public IEnumerable<ProductViewModel> Products { get; set; }
+    }
+}
---
+++ PetShopProject/PetShopProject.Core/ViewModels/ProductViewModels/ProductViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetShopProject.ViewModels.ProductViewModels
+{
+    public class ProductViewModel
+    {
+        /// <summary>
+        /// Product identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Product name
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Product description
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Product price
+        /// </summary>
+        public string Price { get; set; }
+
+        /// <summary>
+        /// Product image url
+        /// </summary>
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
