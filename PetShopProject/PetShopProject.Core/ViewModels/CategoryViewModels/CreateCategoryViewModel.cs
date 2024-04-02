fc7a948696b5d6f786f4fd0fd2ddc2eca9751d8a
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-04-02 20:10:24 +0300

    Adding CreateCategoryViewMolde for creating new category

---
+++ PetShopProject/PetShopProject.Core/ViewModels/CategoryViewModels/CreateCategoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static PetShopProject.Common.EntityValidationsConstants.CategoryValidations;
+using static PetShopProject.Common.EntityValidationsErrorMessages;
+
+namespace PetShopProject.ViewModels.CategoryViewModels
+{
+    public class CreateCategoryViewModel
+    {
+        [Required]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength,
+            ErrorMessage = NameCategoryInccorectLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength, 
+            ErrorMessage = DescriptionCategoryInccorectlength)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string AnimalType { get; set; } = string.Empty;
+    }
+}
