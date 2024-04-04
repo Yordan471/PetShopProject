31ec01fef9d6a7c25b4d915347e29f799138510f
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-04-04 20:40:09 +0300

    adding CategoryEditViewModel

---
+++ PetShopProject/PetShopProject.Core/ViewModels/CategoryViewModels/CategoryEditViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static PetShopProject.Common.EntityValidationsErrorMessages;
+using static PetShopProject.Common.EntityValidationsConstants.CategoryValidations;
+
+namespace PetShopProject.Core.ViewModels.CategoryViewModels
+{
+    public class CategoryEditViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength, 
+            ErrorMessage = NameCategoryInccorectLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength, 
+            ErrorMessage = DescriptionCategoryInccorectlength)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(AnimalTypeMaxLength, MinimumLength = AnimalTypeMinLength, 
+            ErrorMessage = AnimalTypeCategoryInccorectLength)]
+        public string AnimalType { get; set; } = string.Empty;
+    }
+}
