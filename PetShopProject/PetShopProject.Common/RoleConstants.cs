39078f106ee2894e319fe0fe972f839178a1a8e8
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-04-06 11:18:27 +0300

    Adding more constants

--- PetShopProject/PetShopProject.Common/RoleConstants.cs
+++ PetShopProject/PetShopProject.Common/RoleConstants.cs
@@ -3,5 +3,9 @@
     public static class RoleConstants
     {
         public const string RoleAdming = "Admin";
+        public const string NormalizedRoleAdmin = "ADMIN";
+
+        public const string RoleModerator = "Moderator";
+        public const string NormalizedRoleModerator = "MODERATOR";
     }
 }
