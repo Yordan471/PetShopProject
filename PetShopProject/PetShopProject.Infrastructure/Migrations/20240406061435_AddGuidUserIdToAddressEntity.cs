7aadb4ab551ede9097b59450edbd339573fdf5a5
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-04-06 10:17:31 +0300

    Adding migration for property Guid UserId

---
+++ PetShopProject/PetShopProject.Infrastructure/Migrations/20240406061435_AddGuidUserIdToAddressEntity.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetShopProject.Infrastructure.Migrations
+{
+    public partial class AddGuidUserIdToAddressEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Categories",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                comment: "Category description",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150,
+                oldComment: "Category description");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Addresses",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                comment: "User identifier");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { new Guid("6f45410f-ae12-46bf-a736-429395a0574d"), "51808c16-04e9-4716-846a-31abc6db4a61", "Name", "ADMIN" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("6f45410f-ae12-46bf-a736-429395a0574d"));
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Addresses");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Categories",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                comment: "Category description",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300,
+                oldComment: "Category description");
+        }
+    }
+}
