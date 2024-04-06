5daa091f168aa413d71f0802b0cf63ccb5232848
Yordan Ivanov <110678258+Yordan471@users.noreply.github.com>
2024-04-06 16:50:29 +0300

    Delete MyPetShop directory

--- MyPetShop/MyPetShop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyPetShop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
