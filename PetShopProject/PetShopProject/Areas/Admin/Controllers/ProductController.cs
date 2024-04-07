d15fa6bbf254a82019a9316d96abe9dfc0bb2119
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-04-07 21:30:49 +0300

    Adding Gete and Post actions for Create (Product)

--- PetShopProject/PetShopProject/Areas/Admin/Controllers/ProductController.cs
+++ PetShopProject/PetShopProject/Areas/Admin/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PetShopProject.Core.Contracts;
+using PetShopProject.Core.ViewModels.CategoryViewModels;
 using PetShopProject.Core.ViewModels.ProductViewModels;
+using PetShopProject.Infrastructure.Data.Models;
 using System.Collections.Generic;
 using static PetShopProject.Common.GlobalConstants;
 
@@ -11,12 +13,14 @@ namespace PetShopProject.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IProductService productService;
+        private readonly ICategoryService categoryService;
         private readonly ILogger<ProductController> logger;
 
-        public ProductController(IProductService _productService, ILogger<ProductController> _logger)
+        public ProductController(IProductService _productService, ILogger<ProductController> _logger, ICategoryService _categoryService)
         {
             this.productService = _productService;
             this.logger = _logger;
+            this.categoryService = _categoryService;
         }
 
         public async Task<IActionResult> Index()
@@ -53,6 +57,53 @@ namespace PetShopProject.Areas.Admin.Controllers
             //dogProducts.AddRange(birdProducts);
 
             return View(dogProducts);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var viewModel = new ProductCreateViewModel
+            {
+                Categories = await categoryService.GetAllCategoriesForProductCreationAsync()
+                
+            };
+
+            return View(viewModel);
+        }
+
+        // POST: /Product/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductCreateViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var product = new Product
+                    {
+                        Name = viewModel.Name,
+                        LongDescription = viewModel.LongDescription,
+                        ShortDescription = viewModel.ShortDescription,
+                        Price = viewModel.Price,
+                        ImageUrl = viewModel.ImageUrl,
+                        AnimalType = viewModel.AnimalType,
+                        CategoryId = viewModel.CategoryId
+                    };
+
+                    await productService.CreateProduct(product);
+
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An error occurred while creating a product.");
+                    ModelState.AddModelError("", "An error occurred while creating the product.");
+                }
+            }
+
+            viewModel.Categories = await categoryService.GetAllCategoriesForProductCreationAsync();
+
+            return View(viewModel);
         }
     }
 }
