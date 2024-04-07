1d62b887cc545248dcb8a77ff0cb95754dd15075
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-04-07 21:33:59 +0300

    adding CaegoryForCreateProductViewModel adding ProductCreateViewModel

---
+++ PetShopProject/PetShopProject.Core/ViewModels/CategoryViewModels/CategoryForCreateProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Core.ViewModels.CategoryViewModels
+{
+    /// <summary>
+    /// Category model nested in ProductCreateViewModel for creating product
+    /// </summary>
+    public class CategoryForCreateProductViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ PetShopProject/PetShopProject.Core/ViewModels/ProductViewModels/ProductCreateViewModel.cs
@@ -0,0 +1,72 @@
+﻿using PetShopProject.Core.ViewModels.CategoryViewModels;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using static PetShopProject.Common.EntityValidationsConstants.ProductValidations;
+using static PetShopProject.Common.EntityValidationsErrorMessages;
+
+namespace PetShopProject.Core.ViewModels.ProductViewModels
+{
+    /// <summary>
+    /// Product view model for creating product
+    /// </summary>
+    public class ProductCreateViewModel
+    {
+        /// <summary>
+        /// Product name
+        /// </summary>
+        [Required]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength,
+            ErrorMessage = NameCategoryInccorectLength)]
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Product long description for details
+        /// </summary>
+        [Required]
+        [StringLength(LongDescriptionMaxLength, MinimumLength = LongDescriptionMinLength,
+            ErrorMessage = DescriptionCategoryInccorectlength)]
+        public string LongDescription { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Product short desciption
+        /// </summary>
+        [Required]
+        [StringLength(ShortDescriptionMaxLength, MinimumLength = ShortDescriptionMinLength,
+            ErrorMessage = DescriptionCategoryInccorectlength)]
+        public string ShortDescription { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Price of a product
+        /// </summary>
+        [Required]
+        [Range(PriceMinValue, PriceMaxValue,
+            ErrorMessage = PriceProductInccorectRange)]
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Image url for product
+        /// </summary>
+        [Required]
+        [StringLength(ImageUrlMaxLength, MinimumLength = ImageUrlMinLength,
+            ErrorMessage = ImageUrlProductInccorectLength)]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Animal type
+        /// </summary>
+        [Required]
+        [StringLength(AnimalTypeMaxLength, MinimumLength = AnimalTypeMinLength,
+            ErrorMessage = AnimalTypeCategoryInccorectLength)]
+        public string AnimalType { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Category identifier
+        /// </summary>
+        public int CategoryId { get; set; }
+
+        /// <summary>
+        /// Category collection to choose of
+        /// </summary>
+        public IEnumerable<CategoryForCreateProductViewModel> Categories { get; set; }
+    }
+}
