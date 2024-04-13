f70c95f9fd821e5abc05fb57e8091eb9708eb774
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-04-13 19:22:40 +0300

    Adding several methods in ICartService (Contracts) GetAllCartProductsForUser Update, GetCartItemAsync, Remove, CheckIfCartItemExists AddCartItemToDb, IncreaseQuantityCartItemAsync

---
+++ PetShopProject/PetShopProject.Core/Contracts/ICartService.cs
@@ -0,0 +1,27 @@
+﻿using PetShopProject.Core.ViewModels.CartViewModels;
+using PetShopProject.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Core.Contracts
+{
+    public interface ICartService
+    {
+        public Task<List<CartViewModel>> GetAllCartProductsForUser(Guid userId);
+
+        public Task Update(CartItem cartItem, int quantity);
+
+        public Task<CartItem> GetCartItemAsync(int id);
+
+        public Task Remove(CartItem cartItem);
+
+        public Task<CartItem> CheckIfCartItemExists(Guid id, int productId); 
+
+        public Task AddCartItemToDb(CartItem cartItem);
+
+        public Task IncreaseQuantityCartItemAsync(CartItem cartItem, int quantity);
+    }
+}
