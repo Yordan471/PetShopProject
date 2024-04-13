c3d75fd32a465099ebac78e7b65003d6227db1e7
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-04-13 19:23:17 +0300

    Adding Id in CartViewModel

---
+++ PetShopProject/PetShopProject.Core/ViewModels/CartViewModels/CartViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace PetShopProject.Core.ViewModels.CartViewModels
+{
+    /// <summary>
+    /// Cart view model
+    /// </summary>
+    public class CartViewModel
+    {
+        /// <summary>
+        /// Cart item identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Product identifier
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Name of the product
+        /// </summary>
+        public string ProductName { get; set; }
+
+        /// <summary>
+        /// Quantity of he product to buy
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Price of the product
+        /// </summary>
+        public string Price { get; set; }
+    }
+}
