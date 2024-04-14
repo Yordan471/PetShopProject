b3f2ee9ab3da14a612fdc019a32022d9ade9910c
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-04-14 11:46:27 +0300

    Adding HomeViewModel

---
+++ PetShopProject/PetShopProject.Core/ViewModels/HomeViewModels/HomeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using PetShopProject.Core.ViewModels.ProductViewModels;
+
+namespace PetShopProject.Core.ViewModels.HomeViewModels
+{
+    /// <summary>
+    /// Home view model
+    /// </summary>
+    public class HomeViewModel
+    {
+        /// <summary>
+        /// Animal type ("dog", "cat" etc)
+        /// </summary>
+        public string AnimalType { get; set; }
+
+        /// <summary>
+        /// Category name (WetFood, DryFood etc)
+        /// </summary>
+        public string CategoryName { get; set; }
+
+        /// <summary>
+        /// Collection of products
+        /// </summary>
+        public List<ProductViewModel> Products { get; set; }
+    }
+}
