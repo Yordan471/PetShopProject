f3772b5bf1b1b85b2b54a47485a534dd45be3b52
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-04-14 17:06:15 +0300

    Adding models for Checkout action

---
+++ PetShopProject/PetShopProject.Core/ViewModels/CartViewModels/CartCheckoutViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Core.ViewModels.CartViewModels
+{
+    /// <summary>
+    /// View model for checkout information
+    /// </summary>
+    public class CartCheckoutViewModel
+    {
+        /// <summary>
+        /// Cart item identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Product identifier
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Product name
+        /// </summary>
+        public string ProductName { get; set; }
+
+        /// <summary>
+        /// Price of the product
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Quantity of he product in store
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Total sum to pay
+        /// </summary>
+        public decimal Total { get; set; }
+    }
+}
---
+++ PetShopProject/PetShopProject.Core/ViewModels/OrderViewModels/OrderCheckoutViewModel.cs
@@ -0,0 +1,25 @@
+﻿using PetShopProject.Core.ViewModels.CartViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Core.ViewModels.OrderViewModels
+{
+    /// <summary>
+    /// View model for visualizing checkout information
+    /// </summary>
+    public class OrderCheckoutViewModel
+    {
+        /// <summary>
+        /// Collection of cart items
+        /// </summary>
+        public List<CartCheckoutViewModel> CartItems { get; set; }
+
+        /// <summary>
+        /// Total price of all products in the card
+        /// </summary>
+        public decimal TotalPrice { get; set; }
+    }
+}
