616cb388ab9b7b0ecc414bb1cf5ea39bfcfee77a
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-04-14 11:47:18 +0300

    Adding business logic for HomeService - implementing method GetProductsByAnimalAndCategoryAsync

---
+++ PetShopProject/PetShopProject.Core/Contracts/IHomeService.cs
@@ -0,0 +1,15 @@
+﻿using PetShopProject.Core.ViewModels.HomeViewModels;
+using PetShopProject.Core.ViewModels.ProductViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Core.Contracts
+{
+    public interface IHomeService
+    {
+        public Task<IEnumerable<HomeViewModel>> GetProductsByAnimalAndCategoryAsync();
+    }
+}
---
+++ PetShopProject/PetShopProject.Core/Services/HomeService.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShopProject.Core.Contracts;
+using PetShopProject.Core.ViewModels.HomeViewModels;
+using PetShopProject.Core.ViewModels.ProductViewModels;
+using PetShopProject.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Core.Services
+{
+    public class HomeService : IHomeService
+    {
+        private readonly PetShopDbContext dbContext;
+        private readonly IProductService productService;
+
+        public HomeService(PetShopDbContext _dbContext, IProductService _productService) 
+        {
+            dbContext = _dbContext;
+            productService = _productService;
+        }
+
+        public async Task<IEnumerable<HomeViewModel>> GetProductsByAnimalAndCategoryAsync()
+        {
+            var viewModel = new List<HomeViewModel>();
+
+            var animalTypes = await dbContext.Products
+                .AsNoTracking()
+                .Select(p => p.AnimalType)
+                .Distinct()
+                .ToListAsync();
+
+            foreach (var animalType in animalTypes)
+            {
+                var categories = await dbContext.Products
+                    .AsNoTracking()
+                    .Where(p => p.AnimalType == animalType && p.Category.Products.Any())
+                    .Select(p => p.Category.Name)
+                    .Distinct()
+                    .ToListAsync();
+
+                foreach (var category in categories)
+                {
+                    var products = await dbContext.Products
+                        .AsNoTracking()
+                        .Where(p => p.AnimalType == animalType && p.Category.Name == category)
+                        .Take(3)
+                        .Select(p => new ProductViewModel()
+                        {
+                            Id = p.Id,
+                            Name = p.Name,
+                            AnimalType = p.AnimalType,
+                            ImageUrl = p.ImageUrl,
+                            Description = p.ShortDescription,
+                            Price = p.Price.ToString()
+                        })
+                        .ToListAsync();
+
+                    viewModel.Add(new HomeViewModel
+                    {
+                        AnimalType = animalType,
+                        CategoryName = category,
+                        Products = products
+                    });
+                }
+            }
+
+            return viewModel;
+        }
+    }
+}
