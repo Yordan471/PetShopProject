34745f20592965501fa82d6c2ad12e5394464927
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-04-14 17:07:47 +0300

    adding UpdateUserBankAccountAmountAsync for UserService

---
+++ PetShopProject/PetShopProject.Core/Contracts/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using PetShopProject.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Core.Contracts
+{
+    public interface IUserService
+    {
+        public Task UpdateUserBankAccountAmountAsync(User user, decimal totalPrice);
+    }
+}
---
+++ PetShopProject/PetShopProject.Core/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using PetShopProject.Core.Contracts;
+using PetShopProject.Infrastructure.Data;
+using PetShopProject.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Core.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly PetShopDbContext dbContext;
+
+        public UserService(PetShopDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+        public async Task UpdateUserBankAccountAmountAsync(User user, decimal totalPrice)
+        {
+            user.BankAccountAmount -= totalPrice;
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
