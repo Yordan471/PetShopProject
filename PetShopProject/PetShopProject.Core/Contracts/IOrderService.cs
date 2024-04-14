6cd241186a75c6f31f6409dcec49a27553b6077b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-04-14 17:07:30 +0300

    adding CreateOrderAsync for OrderService

---
+++ PetShopProject/PetShopProject.Core/Contracts/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using PetShopProject.Core.ViewModels.CartViewModels;
+using PetShopProject.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Core.Contracts
+{
+    public interface IOrderService
+    {
+        public Task CreateOrderAsync(Order order);
+    }
+}
---
+++ PetShopProject/PetShopProject.Core/Services/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PetShopProject.Core.Contracts;
+using PetShopProject.Core.ViewModels.CartViewModels;
+using PetShopProject.Core.ViewModels.OrderViewModels;
+using PetShopProject.Infrastructure.Data;
+using PetShopProject.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Core.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly PetShopDbContext dbContext;
+        private readonly ILogger<OrderService> logger;
+
+        public OrderService(PetShopDbContext _dbContect, ILogger<OrderService> _logger) 
+        {
+            dbContext = _dbContect;
+            logger = _logger;
+        }
+
+        public async Task CreateOrderAsync(Order order)
+        {
+            await dbContext.AddAsync(order);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
